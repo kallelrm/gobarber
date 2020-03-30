1263e81cfcb6859df17f7b4b20f57dfff9d6ed12
kallelrm <kallelr@outlook.com>
2020-03-30 13:46:30 -0300

    Criando campos virtuais de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -30,7 +30,7 @@ class AppointmentController {
         canceled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20 || 0,
       include: [
